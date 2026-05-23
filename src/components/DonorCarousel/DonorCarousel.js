fbd560e31c2e3af9271bcaec3c95e80ee40b078c
omas <omas7th@gmail.com>
2026-05-23 14:23:15 +0100

    update

--- src/components/DonorCarousel/DonorCarousel.js
+++ src/components/DonorCarousel/DonorCarousel.js
@@ -8,10 +8,10 @@ const DonorCarousel = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 1000,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 3 } },
       { breakpoint: 768, settings: { slidesToShow: 2 } },
